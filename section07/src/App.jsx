2d506d0ce75589c686015e73858b625659cab988
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-26 23:21:22 +0900

    d

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -2,8 +2,16 @@ import "./App.css";
 import Header from "./components/header";
 import List from "./components/List";
 import Editer from "./components/Editer";
-
+import { useState } from "react";
+const mockData = [
+    {id:0, isDone: false, content: "React공부하기", Date: New Date().getTime() },
+    {id:1, isDone: false, content: "빨래하기", Date: New Date().getTime() },
+    {id:2, isDone: false, content: "노래 연습하기", Date: New Date().getTime() },
+]   ;
+    
 function App() {
+
+    const [todos, setTodos] = useState([    ]);
     return (
         <div className="App">
             <Header />
