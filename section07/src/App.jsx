c15576d4df7d16770ee69b372ab8ea5ee5afb4e8
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-27 23:45:16 +0900

    v'

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -2,20 +2,48 @@ import "./App.css";
 import Header from "./components/header";
 import List from "./components/List";
 import Editer from "./components/Editer";
-import { useState } from "react";
-const mockData = [
-    {id:0, isDone: false, content: "React공부하기", Date: New Date().getTime() },
-    {id:1, isDone: false, content: "빨래하기", Date: New Date().getTime() },
-    {id:2, isDone: false, content: "노래 연습하기", Date: New Date().getTime() },
-]   ;
-    
-function App() {
+import { useState, useRef } from "react";
 
-    const [todos, setTodos] = useState([    ]);
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "빨래하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "노래 연습하기",
+        date: new Date().getTime(),
+    },
+];
+
+function App() {
+    const [todos, setTodos] = useState(mockData);
+    const idRef = useRef(3);
+
+    const onCreate = (content) => {
+        const newTodo = {
+            id: idRef.current++,
+            isDone: false,
+            content: content,
+            date: new Date().getTime(),
+        };
+
+        setTodos([...todos, newTodo]);
+    };
+
     return (
         <div className="App">
             <Header />
-            <Editer />
+            <Editer onCreate={onCreate} />
             <List />
         </div>
     );
--- section07/src/components/Editer.jsx
+++ section07/src/components/Editer.jsx
@@ -1,9 +1,37 @@
 import "./Editer.css";
-const Editer = () => {
+import { useState, useRef } from "react";
+
+const Editer = ({ onCreate }) => {
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    };
+
+    const onKeydown = (e) => {
+        if (e.keyCode === 13) {
+            onsubmit();
+        }
+    };
+
+    const onsubmit = () => {
+        if (content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    };
     return (
         <div className="Editer">
-            <input type="text" placeholder="새로운 Todo..." />
-            <button>추가</button>
+            <input
+                ref={contentRef}
+                value={content}
+                onKeyDown={onKeydown}
+                onChange={onChangeContent}
+                placeholder="새로운 Todo..."
+            />
+            <button onClick={onsubmit}>추가</button>
         </div>
     );
 };
