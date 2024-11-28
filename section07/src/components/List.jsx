cc0650bd1cf3cd05e002e6f2ba141b09e1e0f6e9
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-28 11:14:17 +0900

    2024-11-28

--- section07/src/components/List.jsx
+++ section07/src/components/List.jsx
@@ -1,14 +1,44 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-const List = () => {
+import { useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredData = () => {
+        if (search === "") {
+            return todos;
+        }
+        return todos.filter((todo) =>
+            todo.content.toLowerCase().includes(search.toLowerCase())
+        );
+    };
+
+    const filteredTodos = getFilteredData();
+
     return (
         <div className="List">
-            <h4>Todo List 😋</h4>
-            <input placeholder="검색어를 입력하세요" />
+            <h4>Todo List 🌱</h4>
+            <input
+                value={search}
+                onChange={onChangeSearch}
+                placeholder="검색어를 입력하세요"
+            />
             <div className="todos_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {filteredTodos.map((todo) => {
+                    return (
+                        <TodoItem
+                            key={todo.id}
+                            {...todo}
+                            onUpdate={onUpdate}
+                            onDelete={onDelete}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
